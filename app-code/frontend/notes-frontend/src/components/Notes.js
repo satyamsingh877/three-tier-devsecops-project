bee3496ea1ea488a6eeedf813c0aa8a5d7647fdb
Mohamed Azarudeen <helix@Mohameds-Air.lan>
2025-07-01 10:16:25 +0530

    Axios changes in frontend

--- app-code/frontend/notes-frontend/src/components/Notes.js
+++ app-code/frontend/notes-frontend/src/components/Notes.js
@@ -12,12 +12,12 @@ const Notes = () => {
     }, []);
 
     const fetchNotes = async () => {
-        const response = await axios.get('https://api.dovz.shop/api/notes/');
+        const response = await axios.get('/api/notes/');
         setNotes(response.data);
     };
 
     const addNote = async () => {
-        const response = await axios.post('https://api.dovz.shop/api/notes/', { title, content });
+        const response = await axios.post('/api/notes/', { title, content });
         setNotes([...notes, response.data]);
         setTitle('');
         setContent('');
