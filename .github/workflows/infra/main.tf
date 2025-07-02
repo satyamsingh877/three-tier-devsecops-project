4a851a64c9a44a4ffb3a6805241a869c138ec017
satyamsingh877 <78309503+satyamsingh877@users.noreply.github.com>
2025-07-03 02:01:03 +0530

    Create main.tf

---
+++ .github/workflows/infra/main.tf
@@ -0,0 +1,38 @@
+# main.tf
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "three_tier_devsecops_project_bucket" {
+  bucket = "three-tier-devsecops-project-bucket-s3"
+
+  tags = {
+    Name = "three-tier-devsecops-project-bucket-s3"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.three_tier_devsecops_project_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "lock_files" {
+  name         = "lock-files"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "lock-files"
+  }
+}
+
